4097e7df11c91dfd337e6bc0d2abb178135a157d
Nafiu Ishaq <63783380+nafiuishaaq@users.noreply.github.com>
2026-02-21 14:21:33 +0000

    implemented the service

--- backend/src/leaderboard/leaderboard.module.ts
+++ backend/src/leaderboard/leaderboard.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Leaderboard } from './entities/leaderboard.entity';
 import { LeaderboardStats } from './entities/leaderboard-stats.entity';
@@ -7,11 +8,12 @@ import { LeaderboardService } from './leaderboard.service';
 import { User } from '../users/entities/user.entity';
 import { LeaderboardController } from './leaderboard.controller';
 import { LeaderboardQueryService } from './leaderboard-query.service';
+import { SpinSettledEventHandler } from './listeners/spin-settled.listener';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Leaderboard, LeaderboardStats, UserLeaderboardStats, User])],
+  imports: [TypeOrmModule.forFeature([Leaderboard, LeaderboardStats, UserLeaderboardStats, User]), CqrsModule],
   controllers: [LeaderboardController],
-  providers: [LeaderboardService, LeaderboardQueryService],
+  providers: [LeaderboardService, LeaderboardQueryService, SpinSettledEventHandler],
   exports: [LeaderboardService],
 })
 export class LeaderboardModule {}
